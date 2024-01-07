f0b3239322dc5bb3cfe71671d79f80859647ea94
engsiam <siam.softwareengineer@gmail.com>
2024-01-07 20:00:42 +0600

    add single link router

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import useCharacter from '../hooks/useCharacter'
 import './Character.css'
+import {useParams} from "react-router";
 
 export default function Character() {
-  const { loading, data, error } = useCharacter()
+  const {id} = useParams();
+  const { loading, data, error } = useCharacter(id)
 
   if (loading)
     return (
@@ -29,11 +31,15 @@ export default function Character() {
         <h3>{data.character.name}</h3>
         <p>{data.character.gender}</p>
         <div className="Character-episode">
-          {data.character.episode.map((episode) => (
-            <p key={episode.id}>
-              {episode.name} - {episode.episode}
-            </p>
-          ))}
+          {data.character.episode.map((episode) => {
+            return (
+
+              <div key={episode.id}>
+              {episode.name} - <b>{episode.episode}</b>
+              </div>
+            );
+            
+          })}
         </div>
       </div>
     </div>
